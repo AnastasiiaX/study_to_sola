83de300555a2a8690007c6766c8e592133b9f33d
Anastasiia Kosareva <anastasiya.kosareva@gmail.com>
2025-03-20 09:04:22 +0200

    extra fix

--- function_app.py
+++ function_app.py
@@ -16,8 +16,7 @@ USERS_COURSES_URL = os.getenv("USERS_COURSES_URL")
 app = func.FunctionApp()
 
 
+@app.function_name(name="myTimer")
 @app.timer_trigger(schedule="0 0 0 29 2 *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     fetch(CLIENT_ID, CLIENT_SECRET, TOKEN_URL, USERS_COURSES_URL)
-
-    logging.info('Python timer trigger function executed.')
