384e7edb9642e64744e5e1eb7acb38fd571f5bc9
Anastasiia Kosareva <anastasiya.kosareva@gmail.com>
2025-03-20 09:27:35 +0200

    fix

--- function_app.py
+++ function_app.py
@@ -16,7 +16,7 @@ USERS_COURSES_URL = os.getenv("USERS_COURSES_URL")
 app = func.FunctionApp()
 
 
-@app.function_name(name="myTimer")
-@app.timer_trigger(schedule="0 0 0 29 2 *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
-def timer_trigger(myTimer: func.TimerRequest) -> None:
+@app.function_name(name="studyToSola")
+@app.timer_trigger(schedule="0 0 0 29 2 *", arg_name="studyToSola", run_on_startup=True, use_monitor=False)
+def timer_trigger(studyToSola: func.TimerRequest) -> None:
     fetch(CLIENT_ID, CLIENT_SECRET, TOKEN_URL, USERS_COURSES_URL)
